5b4b56a86a602d6fcebe5155216f2cf1f245764d
Ian Maynard <relreothegreat@gmail.com>
2021-03-18 12:17:10 -0700

    Camera Update and Water Obstacle
    I added camera tracking to the player so now it should follow the player around and be in a much better position. This position is easily editable on the camera object.
    
    Made the water obstacle larger and added a script that kills and respawns the player if they touch it.

---
+++ Maze Demo/Assets/Scripts/DeathManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathManager : MonoBehaviour
+{
+    public Transform respawnPoint;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            StartCoroutine(Respawn(3.0f, other.gameObject));
+        }
+    }
+
+    private IEnumerator Respawn(float delay, GameObject obj)
+    {
+        obj.SetActive(false);
+        yield return new WaitForSeconds(delay);
+        obj.transform.position = respawnPoint.position;
+        obj.SetActive(true);
+
+
+    }
+}
